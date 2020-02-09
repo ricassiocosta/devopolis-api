5707bd5061af31131b2db01f1e31311430deef5d
ricassiocosta <riksiocosta@gmail.com>
2020-02-09 15:38:45 -0300

    now devs can create posts with image upload

---
+++ backend/src/controllers/PostController.js
@@ -0,0 +1,26 @@
+const Post = require('../models/Post')
+const Dev = require('../models/Dev')
+
+module.exports = {
+    async store(req, res) {
+        const { filename } = req.file
+        const { title, post } = req.body
+        const { dev_id } = req.headers
+
+        const dev = await Dev.findById(dev_id)
+        if(!dev) {
+            return res.status(400).json({
+                error: 'user does not exist'
+            })
+        }
+
+        const publication = await Post.create({
+            author: dev_id,
+            thumbnail: filename,
+            title,
+            post
+        })
+
+        return res.json(publication)
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,19 @@
 const { Router } = require('express')
+const multer = require('multer')
+const uploadConfig = require('./config/upload')
+
 const DevController = require('./controllers/DevController')
 const SearchController = require('./controllers/SearchController')
+const PostController = require('./controllers/PostController')
 
 const routes = Router()
+const upload = multer(uploadConfig)
 
 routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store)
 
 routes.get('/search', SearchController.index)
+
+routes.post('/post', upload.single('thumbnail'),PostController.store)
 
 module.exports = routes
