6d0a656050fbbddcd2027dce71941362a15f44cd
ricassiocosta <riksiocosta@gmail.com>
2020-02-09 14:29:23 -0300

    devs now can be searched by techs

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,16 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+    async index(req, res) {
+        const { techs } = req.query
+        const techsArray = parseStringAsArray(techs)
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray
+            }
+        })
+        return res.json({ devs })
+    }
+}
