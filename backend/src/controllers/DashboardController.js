22c159f9e52238126f184a7ce5d47e42fbd4854d
ricassiocosta <riksiocosta@gmail.com>
2020-02-09 19:45:43 -0300

    devs now can have followers

---
+++ backend/src/controllers/DashboardController.js
@@ -0,0 +1,8 @@
+const Post = require('../models/Post')
+const FriendList = require('../models/FriendList')
+
+module.exports = {
+    async index(req, res) {
+
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,8 @@ const uploadConfig = require('./config/upload')
 const DevController = require('./controllers/DevController')
 const SearchController = require('./controllers/SearchController')
 const PostController = require('./controllers/PostController')
+const ProfileController = require('./controllers/ProfileController')
+const DashboardController = require('./controllers/DashboardController')
 
 const routes = Router()
 const upload = multer(uploadConfig)
@@ -15,5 +17,10 @@ routes.post('/devs', DevController.store)
 routes.get('/search', SearchController.index)
 
 routes.post('/post', upload.single('thumbnail'),PostController.store)
+
+routes.get('/dashboard', DashboardController.index)
+
+routes.get('/profile', ProfileController.index)
+routes.put('/profile/:friend_id', DevController.update)
 
 module.exports = routes
