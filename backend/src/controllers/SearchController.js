cd7e388ebb621546ceaf52971b99b5f01ca383ce
Lucas Gomes <lucasdeoliveira887@gmail.com>
2020-02-11 19:57:02 -0300

    Ran the lint:fix script

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -1,16 +1,16 @@
-const Dev = require('../models/Dev')
-const parseStringAsArray = require('../utils/parseStringAsArray')
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
-    async index(req, res) {
-        const { techs } = req.query
-        const techsArray = parseStringAsArray(techs)
+  async index(req, res) {
+    const { techs } = req.query;
+    const techsArray = parseStringAsArray(techs);
 
-        const devs = await Dev.find({
-            techs: {
-                $in: techsArray
-            }
-        })
-        return res.json({ devs })
-    }
-}
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+    });
+    return res.json({ devs });
+  },
+};
