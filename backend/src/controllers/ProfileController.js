8354b80d2ab4b4ae6c7fa3bf2126848f07eb6c56
ricassiocosta <riksiocosta@gmail.com>
2020-02-12 18:27:07 -0300

    dev ids are now sent in headers

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,10 +1,9 @@
 const Post = require('../models/Post')
-const Dev = require('../models/Dev')
 const mongoose = require('mongoose')
 
 module.exports = {
     async index(req, res) {
-        const { dev_id } = req.query
+        const { dev_id } = req.headers
 
         const posts = await Post.find({
             author: mongoose.Types.ObjectId(dev_id)
