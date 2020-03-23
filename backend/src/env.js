eca4f3fb08f3835dcf29231fd8910e46e1995741
Lucas Gomes <lucasdeoliveira887@gmail.com>
2020-03-23 20:02:25 -0300

    Added PORT as env variable, moved index.js to the root and removed npm from package.json

--- backend/src/env.js
+++ backend/src/env.js
@@ -1,4 +1,5 @@
 module.exports = {
   DATABASE_USERNAME: process.env.DATABASE_USERNAME || 'root',
-  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'root'
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'root',
+  PORT: process.env.PORT || 5000
 }
